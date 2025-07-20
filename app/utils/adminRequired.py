ac3973661ae5dea78b02f78e337fe0fdce287597
José Gallardo <tory002254@gmail.com>
2025-07-20 16:41:40 +0200

    fix: Correct admin access check logic in admin_required decorator

--- app/utils/adminRequired.py
+++ app/utils/adminRequired.py
@@ -7,7 +7,7 @@ from flask_login import current_user
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not current_user.is_authenticated or not current_user.is_admin:
+        if not current_user.is_authenticated and not current_user.is_admin:
             print('Acceso denegado. Debes ser administrador para ver esta página.')
             return redirect(url_for('main.inicio'))
         return f(*args, **kwargs)
